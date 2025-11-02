dc716b0bcbf9ecdb6bdfe4a5fff4b7bb5211ca35
Kittidet Wichaidit <6610685098@student.tu.ac.th>
2025-11-02 23:17:36 +0700

    Update TU_Reports/settings.py for deployment

--- TU_Reports/TU_Reports/settings.py
+++ TU_Reports/TU_Reports/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a)069yb3z*e#g840^*-+q8v(_^w-wjr6)_8v*s12xpgpx258zx'
+default-key = 'django-insecure-a)069yb3z*e#g840^*-+q8v(_^w-wjr6)_8v*s12xpgpx258zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
