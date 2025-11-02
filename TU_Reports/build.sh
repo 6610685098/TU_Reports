091320979495b6e7ca3363bb7acb7d84967d5a3d
Kittidet Wichaidit <6610685098@student.tu.ac.th>
2025-11-02 22:47:47 +0700

    Add build script for Render

---
+++ TU_Reports/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# หยุดสคริปต์ทันทีถ้ามีคำสั่งไหนล้มเหลว
+set -o errexit
+
+# 1. ติดตั้ง Dependencies
+pip install -r requirements.txt
+
+# 2. รวบรวม Static Files
+python manage.py collectstatic --noinput
+
+# 3. รัน Database Migrations
+python manage.py migrate
