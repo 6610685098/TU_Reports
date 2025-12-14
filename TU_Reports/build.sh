01d2358e960525a0337ed83baa0c3713adb819d4
Kittidet Wichaidit <6610685098@student.tu.ac.th>
2025-12-15 02:54:20 +0700

    Add build.sh

---
+++ TU_Reports/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# หยุดสคริปต์ทันทีถ้ามีคำสั่งไหนล้มเหลว
+set -o errexit
+
+# 1. ติดตั้ง Dependencies
+pip install -r requirements.txt
+
+# 2. รวบรวม Static Files
+python manage.py collectstatic --noinput
+
+# 3. รัน Database Migrations
+python manage.py migrate
+
+# 4. โหลดข้อมูลตัวอย่าง
+python manage.py bootstrap_demo_data
+
+# 5. สร้าง Superuser 
+python manage.py createsuperuser --username admin --email "cn331@email.com" --noinput || true
